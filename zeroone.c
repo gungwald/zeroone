9c2d6c41d5f378f55368bba88e77bae8bf1997d8
Bill Chatfield <bill_chatfield@yahoo.com>
2020-12-14 23:43:45 -0500

    Implemented wait for key press

--- zeroone.c
+++ zeroone.c
@@ -6,79 +6,71 @@
 
 #include "vroot.h"
 
-long int delta();
+static char *describeDisplay(char *display);
+static long int delta();
+static Window getWindow(Display *display, int screenNumber, Window root, int argc, char *argv[]);
 
 int main(int argc, char *argv[]) {
-	Display *dpy;
+	Display *display;
 	Window root, win;
 	XWindowAttributes winAttributes;
 	GC graphicsContext;
 	Font sonyFont;
 	XColor redx, reds;
 	long startX, startY, x, y;
-	XEvent e;
-	long blackPixel, whitePixel;
+	XEvent event;
+	char *displayEnv;
+	int screenNumber;
+	Screen *screen;
 
-	dpy = XOpenDisplay(getenv("DISPLAY"));
-	root = DefaultRootWindow(dpy);
-	blackPixel = BlackPixel(dpy, DefaultScreen (dpy));
-	whitePixel = WhitePixel(dpy, DefaultScreen (dpy));
-
-	if (argc > 1 && strcmp(argv[1], "-root")) {
-		win = root;
-	} else {
-		/* create, name, and map window */
-		win = XCreateSimpleWindow(dpy, root, 200, 200, 400, 400, 1, 
-				whitePixel, blackPixel);
-		XStoreName(dpy, win, "ZeroOne");
-		XSelectInput(dpy, win, StructureNotifyMask);
-		XMapRaised(dpy, win);
-		do {
-			XWindowEvent(dpy, win, StructureNotifyMask, &e);
-		} while (e.type != MapNotify);
+	displayEnv = getenv("DISPLAY");
+	display = XOpenDisplay(displayEnv);
+	if (display == NULL) {
+		fprintf(stderr, "Failed to open display: %s\n", describeDisplay(displayEnv));
+		exit(EXIT_FAILURE);
 	}
 
-	/* get attributes of the root window */
-	XGetWindowAttributes(dpy, win, &winAttributes);
+	root = DefaultRootWindow(display);
+	screenNumber = DefaultScreen(display);
+	screen = DefaultScreenOfDisplay(display);
+	win = getWindow(display, screenNumber, root, argc, argv);
 
-	/* create a GC for drawing in the window */
-	graphicsContext = XCreateGC(dpy, win, 0, NULL);
-
-	sonyFont = XLoadFont(dpy, "-sony-*-*-*-*-*-*-*-*-*-*-*-*-*");
-	XSetFont(dpy, graphicsContext, sonyFont);
-
-	/* allocate the red color */
-	XAllocNamedColor(dpy, DefaultColormapOfScreen(DefaultScreenOfDisplay(dpy)),
-			"red", &reds, &redx);
-
-	/* set foreground color */
-	XSetForeground(dpy, graphicsContext, reds.pixel);
+	XGetWindowAttributes(display, win, &winAttributes);
+	graphicsContext = XCreateGC(display, win, 0, NULL);
+	sonyFont = XLoadFont(display, "-sony-*-*-*-*-*-*-*-*-*-*-*-*-*");
+	XSetFont(display, graphicsContext, sonyFont);
+	XAllocNamedColor(display, DefaultColormapOfScreen(screen), "red", &reds, &redx);
+	XSetForeground(display, graphicsContext, reds.pixel);
 
 	x = random() % winAttributes.width;
 	y = random() % winAttributes.height;
 
-	while (1) {
+	XSelectInput(display, win, KeyPressMask);
+
+	/* Loop until a key is pressed. */
+	while (! XCheckTypedEvent(display, KeyPress, &event)) {
 		printf("x=%ld y=%ld\n", x, y);
-		XDrawString(dpy, win, graphicsContext, x, y, " ", 1);
 		char digit = random() % 2 + '0';
-		XDrawString(dpy, win, graphicsContext, x, y, &digit, 1);
+		XDrawString(display, win, graphicsContext, x, y, &digit, 1);
 		x += delta();
 		y += delta();
 
 		if (x < 0 || y < 0 || x >= winAttributes.width || y >= winAttributes.height) {
 			puts("clear");
-			XClearWindow(dpy, win);
+			XClearWindow(display, win);
 			x = random() % winAttributes.width;
 			y = random() % winAttributes.height;
 		}
-
-		/* flush changes and sleep */
-		XFlush(dpy);
+		XFlush(display);
 		usleep(1000000);
 	}
-
-	XCloseDisplay(dpy);
+	XCloseDisplay(display);
 	return EXIT_SUCCESS;
+}
+
+char *describeDisplay(char *display)
+{
+	return display==NULL?"Default":display;
 }
 
 long int delta()
@@ -99,4 +91,26 @@ long int delta()
 		break;
 	}
 	return change;
+}
+
+Window getWindow(Display *display, int screenNumber, Window root, int argc, char *argv[])
+{
+	Window win;
+	XEvent event;
+	long blackPixel, whitePixel;
+
+	blackPixel = BlackPixel(display, screenNumber);
+	whitePixel = WhitePixel(display, screenNumber);
+
+	if (argc > 1 && strcmp(argv[1], "-root")) {
+		win = root;
+	} else {
+		win = XCreateSimpleWindow(display, root, 200, 200, 400, 400, 1, whitePixel, blackPixel);
+		XStoreName(display, win, "ZeroOne");
+		XSelectInput(display, win, StructureNotifyMask);
+		XMapRaised(display, win);
+		do {
+			XWindowEvent(display, win, StructureNotifyMask, &event);
+		} while (event.type != MapNotify);
+	}
 }
